7d3fdd034947bc68c179f788fb1e8287229833e5
billschnoebelen <bill@circuitcase.com>
2022-10-12 10:56:24 -0700

    tried to get gmail to work

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-
 // from https://www.youtube.com/watch?v=jsrtxnFpGaw
-
 
 // Use at least Nodemailer v4.1.0
 const nodemailer = require("nodemailer");
 
 const recipient = "bill@circuitcase.com";
 
+const host = "smtp.gmail.com";
+const user = "bill@circuitcase.com";
+const password = "GMgreen3451$#";
+
+// const host = "smtp.ethereal.email";
+// const user = "braeden57@ethereal.email";
+// const password = "F9WWjnCpCckzVEKBSj";
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
+  host: `${host}`,
   port: 587,
   auth: {
-    user: "braeden57@ethereal.email",
-    pass: "F9WWjnCpCckzVEKBSj",
+    user: `${user}`,
+    pass: `${password}`,
   },
 });
 
@@ -35,8 +41,3 @@ transporter.sendMail(message, (err, info) => {
   // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 });
-
-// async function sendEmail({ from, to, subject, html }) {
-//     const transporter = nodemailer.createTransport(config.smtpOptions);
-//     await transporter.sendMail({ from, to, subject, html });
-//   }
